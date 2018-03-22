2587719f04a520a915c92cdf08f94eaa62e02a9e
Kian <kiangroenen@gmail.com>
2018-03-22 14:11:05 +0100

    Finished lab 5, added display of increasing font when answer selected

--- Week5/sockets/public/javascripts/app.js
+++ Week5/sockets/public/javascripts/app.js
@@ -1,3 +1,5 @@
+var size1 = 10;
+var size2 = 10;
 var url = "/";
 var primus = Primus.connect(url, {
   reconnect: {
@@ -8,25 +10,58 @@ var primus = Primus.connect(url, {
 });
 
 primus.on('data', function(data) {
-	var title = document.querySelector(".title--live");
-	var a1 = document.querySelector(".answer1--live");
-	var a2 = document.querySelector(".answer2--live");
+	if (data.action) {
+		var title = document.querySelector(".title--live");
+		var a1 = document.querySelector(".answer1--live");
+		var a2 = document.querySelector(".answer2--live");
 
-	if (title && a1 && a2) {
-		title.innerHTML = data.action[0];
-		a1.innerHTML = data.action[1];
-		a2.innerHTML = data.action[2];
+		if (title && a1 && a2) {
+			title.innerHTML = data.action[0];
+			a1.innerHTML = data.action[1];
+			a2.innerHTML = data.action[2];
+		}
+	}
+	
+	if (data.add1) {
+		var a1 = document.querySelector(".answer1--live");
+		a1.style.fontSize = size1 + 10 + "px";
+		size1 = size1 + 10;
+		console.log(size1);
+	}
+
+	if (data.add2) {
+		var a2 = document.querySelector(".answer2--live");
+		a1.style.fontSize = size2 + 10 + "px";
+		size2 = size2 + 10;
+		console.log(size2);
 	}
 });
 
 // klik naar server sturen
-document.querySelector('.submit').addEventListener('click', function(e){
-	var q = document.querySelector('.question').value;
-	var a1 = document.querySelector('.answer1').value;
-	var a2 = document.querySelector('.answer2').value;
-	var string = [q, a1, a2];
-	console.log(string);
-	primus.write({ action: string });
+if (document.querySelector('.submit') !== null) {
+	document.querySelector('.submit').addEventListener('click', function(e){
+		var q = document.querySelector('.question').value;
+		var a1 = document.querySelector('.answer1').value;
+		var a2 = document.querySelector('.answer2').value;
+		var string = [q, a1, a2];
+		console.log(string);
+		primus.write({ action: string });
 
-	e.preventDefault();
-});
+		e.preventDefault();
+	});
+};
+
+if (document.querySelector('.answer1--live') !== null) {
+	document.querySelector('.answer1--live').addEventListener('click', function(e) {
+		primus.write({add1: 'answer1'});
+		e.preventDefault();
+	});
+};
+
+if (document.querySelector('.answer2--live') !== null) {
+	document.querySelector('.answer2--live').addEventListener('click', function(e) {
+		primus.write({add2: 'answer2'});
+		e.preventDefault();
+	});
+};
+
